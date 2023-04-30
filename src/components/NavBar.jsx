f37b1f3eb4dd518ef4155644d40d933897568ae0
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-30 01:43:30 -0300

    feat: adiciona funcionalidade para a sideBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import imgListSvg from '../images/list.svg'
 import styles from './NavBar.module.css'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
+  const [sideBar, setSideBar] = React.useState(false);
+  const showSideBar = () => setSideBar(!sideBar)
+
+  React.useEffect(() => {
+    document.body.style.overflow = 'auto'
+    if (sideBar) {
+      document.body.style.overflow = 'hidden'
+    }
+  }, [sideBar])
+
   return (
     <div>
       <div className={styles.line}></div>
       <div className={styles.container}>
         <h1>gcontrole</h1>
-        <img src={imgListSvg} alt="img list" />
+        <button 
+          style={{ width: 30, height: 30, background: 'None' }}
+          onClick={ showSideBar }
+        >
+          <img style={{ width: 30, height: 30 }} src={imgListSvg} alt="img list" />
+        </button>
       </div>
+      { sideBar && 
+      <section className={ styles.sideBar}>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/analytics/payment">
+          <p>Análise de pagamentos</p>
+        </Link>
+        <Link to="/analytics/category">
+          <p>Análise de categoria</p>
+        </Link>
+      </section>
+      }
     </div>
   )
+
 }
 
 export default NavBar
