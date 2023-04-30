13def4e493d172df5c4445e66760494ba99204ac
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-29 22:30:10 -0300

    feat: cria paginas de analytics

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ManagementControl from "./page/ManagementControl";
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
+import AnalyticsPayment from "./components/analytics/AnalyticsPayment";
+import AnalyticsCategory from "./components/analytics/AnalyticsCategory";
 
 function App() {
   return (
     <ControlProvider>
       <Routes>
         <Route path="/" element={ <ManagementControl />} />
-        <Route path="/analytics/payment" element={ <ManagementControl />} />
-        <Route path="/analytics/category" element={ <ManagementControl />} />
+        <Route path="/analytics/payment" element={ <AnalyticsPayment />} />
+        <Route path="/analytics/category" element={ <AnalyticsCategory />} />
         <Route path="*" element={ <NotFound />}/>
       </Routes>
     </ControlProvider>
