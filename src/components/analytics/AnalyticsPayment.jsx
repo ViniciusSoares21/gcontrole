37eb62b6159405ada14b086b78955c9aacc82dbf
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-30 01:02:32 -0300

    feat: adiciona grafico de tipo de pagamento

--- src/components/analytics/AnalyticsPayment.jsx
+++ src/components/analytics/AnalyticsPayment.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import NavBar from '../NavBar'
+import { Chart } from "react-google-charts";
+import styles from './AnalyticsPayment.module.css'
 
 function AnalyticsPayment() {
+
+  const getItems = JSON.parse(localStorage.getItem('listCards'));
+  const qtyCredit = getItems
+    .filter((item) => item.payment === 'Cartão de crédito').length;
+
+  const qtyDebit = getItems
+    .filter((item) => item.payment === 'Cartão de débito').length;
+
+  const qtyTicket = getItems
+    .filter((item) => item.payment === 'Boleto').length;
+
+  const qtyMoney = getItems
+    .filter((item) => item.payment === 'Dinheiro').length;
+
+  const data = [
+    ["category", "quantity"],
+    ["Cartão de crédito", qtyCredit],
+    ["Cartão de débito", qtyDebit],
+    ["Boleto", qtyTicket],
+    ["Dinheiro", qtyMoney],
+  ];
+
+  const options = {
+    width: 366,
+    height: 200,
+    pieHole: 0.6,
+    is3D: false,
+    chartArea: { left:35, width: 250, height: 200 },
+    legend: {
+      alignment: 'center', 
+      position: 'right', 
+      textStyle: {color: 'black', fontSize: 8 },
+    },
+    pieSliceTextStyle: { fontSize: 10},
+
+  };
+
   return (
     <main>
       <NavBar />
+      <div className={ styles.chart}>
+        <Chart
+          chartType="PieChart"
+          data={data}
+          options={options}
+      />
+      </div>
     </main>
   )
 }
