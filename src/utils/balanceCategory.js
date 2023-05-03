e93f14f89283c52357fdbdc34348e9cde3005044
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-03 20:54:24 -0300

    fix: arruma função para fazer o balanço apena quando o localStorage estiver algum item

--- src/utils/balanceCategory.js
+++ src/utils/balanceCategory.js
@@ -1,20 +1,25 @@
 
 const balanceCategory = (category) => {
   const getItems = JSON.parse(localStorage.getItem('listCards'));
-  const Debit = getItems
-  .filter((item) => item.category === category);
-
   let sumDebit = '0,00';
 
-  if (Debit.length > 0) {
-    sumDebit = Debit.reduce((acc, cur) => {
-      const priceCur = Number(cur.price.replace(',', '.'));
-      const priceAcc = Number(String(acc).replace(',', '.'));
-      return (priceAcc + priceCur).toFixed(2).replace('.', ',');
-    }, 0);
-  };
+  if (getItems) {
+    const Debit = getItems
+    .filter((item) => item.category === category);
+  
+  
+    if (Debit.length > 0) {
+      sumDebit = Debit.reduce((acc, cur) => {
+        const priceCur = Number(cur.price.replace(',', '.'));
+        const priceAcc = Number(String(acc).replace(',', '.'));
+        return (priceAcc + priceCur).toFixed(2).replace('.', ',');
+      }, 0);
+    };
+  
+    return {sumDebit, qty: Debit.length }
+  }
 
-  return {sumDebit, qty: Debit.length }
+  return {sumDebit, qty: 0 }
 }
 
 export default balanceCategory;
