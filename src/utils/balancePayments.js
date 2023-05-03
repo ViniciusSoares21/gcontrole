3838dd820840626355c256dcea39f9d0e872560e
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-03 20:57:05 -0300

    fix: arruma função para fazer o balanço apena quando o localStorage estiver algum item

--- src/utils/balancePayments.js
+++ src/utils/balancePayments.js
@@ -1,31 +1,8 @@
 
 const balance = (payment) => {
   const getItems = JSON.parse(localStorage.getItem('listCards'));
-  const Debit = getItems
-  .filter((item) => item.payment === payment);
-
-  const debitNegative = Debit.filter((item) => item.category !== 'Salário');
   let sumDebitNegative = '0,00';
-
-  if (debitNegative.length > 0) {
-    sumDebitNegative = debitNegative.reduce((acc, cur) => {
-      const priceCur = Number(cur.price.replace(',', '.'));
-      const priceAcc = Number(String(acc).replace(',', '.'));
-      return (priceAcc + priceCur).toFixed(2).replace('.', ',');
-  }, 0);
-  };
-
-  const debitPositive = Debit.filter((item) => item.category === 'Salário');
   let sumDebitPositive = '0,00';
-
-  if (debitPositive.length > 0) {
-    sumDebitPositive = debitPositive.reduce((acc, cur) => {
-      const priceCur = Number(cur.price.replace(',', '.'));
-      const priceAcc = Number(String(acc).replace(',', '.'));
-      return (priceAcc + priceCur).toFixed(2).replace('.', ',');
-  }, 0);
-  };
-    
   const sumLiquid = (
     Number(
       sumDebitPositive.replace(',', '.')
@@ -33,8 +10,34 @@ const balance = (payment) => {
       sumDebitNegative.replace(',', '.')
     )
   );
+  if (getItems) {
+    const Debit = getItems
+    .filter((item) => item.payment === payment);
+  
+    const debitNegative = Debit.filter((item) => item.category !== 'Salário');
+  
+    if (debitNegative.length > 0) {
+      sumDebitNegative = debitNegative.reduce((acc, cur) => {
+        const priceCur = Number(cur.price.replace(',', '.'));
+        const priceAcc = Number(String(acc).replace(',', '.'));
+        return (priceAcc + priceCur).toFixed(2).replace('.', ',');
+    }, 0);
+    };
+  
+    const debitPositive = Debit.filter((item) => item.category === 'Salário');
+  
+    if (debitPositive.length > 0) {
+      sumDebitPositive = debitPositive.reduce((acc, cur) => {
+        const priceCur = Number(cur.price.replace(',', '.'));
+        const priceAcc = Number(String(acc).replace(',', '.'));
+        return (priceAcc + priceCur).toFixed(2).replace('.', ',');
+    }, 0);
+    };
+    return { sumDebitNegative, sumDebitPositive, sumLiquid, qty: Debit.length }
+  }
 
-  return { sumDebitNegative, sumDebitPositive, sumLiquid, qty: Debit.length }
+  return { sumDebitNegative, sumDebitPositive, sumLiquid, qty: 0 }
+
 }
 
 export default balance;
