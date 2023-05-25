eee4a38d89b75c763b9145bf9c87ca56a3ba9994
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-25 17:31:19 -0300

    fix: retira importação

--- src/page/Donation.jsx
+++ src/page/Donation.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import styles from './Donation.module.css'
-import Footer from '../components/Footer'
 
 function Donation() {
   return (
