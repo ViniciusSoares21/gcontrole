b33b4952b81c35e0cc04455637fdca84abf8f112
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-25 17:10:18 -0300

    refactory-styles: arruma page NotFaund para ser responsivo em layouts de [celular e tablet]

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,11 +3,30 @@ import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>
-      <h1>Pagína não encotrada</h1>
-      <Link to="/">
-        <h3>Voltar para HOME</h3>
-      </Link>
+    <div 
+      style={{
+        display: 'flex',
+        margin: 'auto',
+        width: '100vw',
+        height: '100vh',
+        textAlign: 'center',
+        justifContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(35, 147, 247, 1)',
+        color: 'white'
+      }}
+    >
+      <div 
+        style={{
+          width: '94%',
+          margin: 'auto'
+        }}
+      >
+        <h1>Pagína não encotrada</h1>
+        <Link to="/">
+          <h3>Voltar para HOME</h3>
+        </Link>
+      </div>
     </div>
   )
 }
