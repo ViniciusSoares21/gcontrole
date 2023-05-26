7525d1a4b6b6999b59f9c48b7d6bd498bfe10b87
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-26 08:51:18 -0300

    feat: implemeta grafico de rosca para a pagina de analise por categoria

--- src/components/analytics/ChartCategory.jsx
+++ src/components/analytics/ChartCategory.jsx
@@ -4,26 +4,45 @@ import styles from './AnalyticsPayment.module.css'
 import balanceCategory from '../../utils/balanceCategory';
 
 function ChartCategory() {
+  const salary = balanceCategory('Salário').sumDebit.replace(',', '.');
+  const food = balanceCategory('Alimentação').sumDebit.replace(',', '.');
+  const transport = balanceCategory('Transporte').sumDebit.replace(',', '.');
+  const health = balanceCategory('Saúde').sumDebit.replace(',', '.');
+  const leisure = balanceCategory('Lazer').sumDebit.replace(',', '.');
+  const Shopping = balanceCategory('Compras').sumDebit.replace(',', '.');
+
   const data = [
-    ["category", "quantity", { role: 'style' }],
-    ["Salário", balanceCategory('Salário').qty, '21E229'],
-    ["Alimentação", balanceCategory('Alimentação').qty, 'FF6E1C'],
-    ["Transporte", balanceCategory('Transporte').qty, '245BDB'],
-    ["Saúde", balanceCategory('Saúde').qty, 'ED1919'],
-    ["Lazer", balanceCategory('Lazer').qty, 'FFE817'],
-    ["Compras", balanceCategory('Compras').qty, 'EA16BB'],
+    ["category", "quantity"],
+    ["Salário", Number(salary)],
+    ["Alimentação", Number(food)],
+    ["Transporte", Number(transport)],
+    ["Saúde", Number(health)],
+    ["Lazer", Number(leisure)],
+    ["Compras", Number(Shopping)],
   ];
+
+  /* const options = {
+    height: 200,
+    legend: { position: 'none' },
+    chartArea: { width: 500, height: 130, top: 20 },
+    pieHole: 0.4,
+    colors: ['21E229', 'FF6E1C', '245BDB', 'ED1919', 'FFE817', 'EA16BB']
+  }; */
 
   const options = {
     height: 200,
+    pieHole: 0.6,
     legend: { position: 'none' },
-    chartArea: { width: 280, height: 130, top: 20 },
+    is3D: false,
+    chartArea: { width: '94%', height: '94%' },
+    pieSliceTextStyle: { fontSize: 10, color: 'black', bold: true, },
+    colors: ['21E229', 'FF6E1C', '245BDB', 'ED1919', 'FFE817', 'EA16BB']
   };
 
   return (
     <div className={ styles.chart}>
       <Chart
-        chartType="ColumnChart"
+        chartType="PieChart"
         data={data}
         options={options}
       />
