aac3e3b7f9c56da922b6e357a141ca19dc451286
Vinicius Soares <viniciussoares72943@gmail.com>
2023-05-30 13:27:38 -0300

    feat: adiciona ad banner nativo

--- src/page/ManagementControl.jsx
+++ src/page/ManagementControl.jsx
@@ -11,6 +11,8 @@ function ManagementControl() {
       <NavBar />
       <Dashboard />
       <WalletForm />
+      <script async="async" data-cfasync="false" src="//pl19565924.highrevenuegate.com/bd7c3cb4b895934eec2608fefe46f5b9/invoke.js"></script>
+      <div id="container-bd7c3cb4b895934eec2608fefe46f5b9"></div>
       <Content />
       <Footer />
     </div>
